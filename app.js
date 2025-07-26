b4099dcaaff31c24d3b6d3943ea36f7afecba0ea
senafathoni2998 <senaahma2998@gmail.com>
2025-07-26 21:54:27 +0700

    fix: add error handling middleware to manage application errors

--- app.js
+++ app.js
@@ -8,6 +8,14 @@ const app = express();
 app.use(bodyParser.json());
 app.use("/api/places", placesRoutes);
 
+app.use((error, req, res, next) => {
+  if (res.headerSent) {
+    return next(error);
+  }
+  res.status(error.code || 500);
+  res.json({ message: error.message || "An unknown error occurred!" });
+});
+
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
 });
