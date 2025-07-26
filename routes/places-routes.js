7a4616394637b99f085634b9ce477783c5ef22fd
senafathoni2998 <senaahma2998@gmail.com>
2025-07-26 22:00:33 +0700

    fix: standardize error handling in place retrieval routes

--- routes/places-routes.js
+++ routes/places-routes.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const HttpError = require("../models/http-error");
 
 const router = express.Router();
 
@@ -29,9 +30,7 @@ router.get("/:pid", (req, res, next) => {
   const placeId = req.params.pid;
   const place = DUMMY_PLACES.find((p) => p.id === placeId);
   if (!place) {
-    const error = new Error("Could not find a place for the provided id.");
-    error.code = 404;
-    throw error;
+    throw new HttpError("Could not find a place for the provided id.", 404);
   }
 
   res.json({ message: "Fetching place success!", place });
@@ -41,9 +40,9 @@ router.get("/user/:uid", (req, res, next) => {
   const userId = req.params.uid;
   const places = DUMMY_PLACES.filter((p) => p.creator === userId);
   if (places.length === 0) {
-    const error = new Error("Could not find places for the provided user id.");
-    error.code = 404;
-    return next(error);
+    return next(
+      new HttpError("Could not find places for the provided user id.", 404)
+    );
   }
 
   res.json({ message: "Fetching places for user success!", places });
