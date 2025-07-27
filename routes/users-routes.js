42128011237b56fc7c2418204f0ba082dc6fb968
senafathoni2998 <senaahma2998@gmail.com>
2025-07-27 12:36:48 +0700

    feat: add input validation for signup and login routes

--- routes/users-routes.js
+++ routes/users-routes.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const { check } = require("express-validator");
 
 const router = express.Router();
 
@@ -6,8 +7,23 @@ const usersControllers = require("../controllers/users-controllers");
 
 router.get("/", usersControllers.getUsers);
 
-router.post("/signup", usersControllers.signup);
+router.post(
+  "/signup",
+  [
+    check("name").not().isEmpty(),
+    check("email").normalizeEmail().isEmail(),
+    check("password").isLength({ min: 6 }),
+  ],
+  usersControllers.signup
+);
 
-router.post("/login", usersControllers.login);
+router.post(
+  "/login",
+  [
+    check("email").normalizeEmail().isEmail(),
+    check("password").isLength({ min: 6 }),
+  ],
+  usersControllers.login
+);
 
 module.exports = router;
