2ec24444efaac55e8b5622648f3b2d2c4cea2e1c
senafathoni2998 <senaahma2998@gmail.com>
2025-07-27 10:58:15 +0700

    feat: add update and delete routes for places

--- routes/places-routes.js
+++ routes/places-routes.js
@@ -8,6 +8,10 @@ router.get("/:pid", placesControllers.getPlaceById);
 
 router.get("/user/:uid", placesControllers.getPlacesByUserId);
 
+router.patch("/:pid", placesControllers.updatePlace);
+
+router.delete("/:pid", placesControllers.deletePlace);
+
 router.post("/", placesControllers.createPlace);
 
 module.exports = router;
